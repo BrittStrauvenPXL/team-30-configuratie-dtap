8ab00ed454823737c5a3f7185750dd51b3031c05
BrittStrauvenPXL <116876064+BrittStrauvenPXL@users.noreply.github.com>
2023-12-06 11:46:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config({ path: `${__dirname}/config.env` });
 const app = require("./app")
 
 const port = process.env.PORT || 3000;
