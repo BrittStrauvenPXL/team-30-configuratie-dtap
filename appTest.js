2c536b4d30e38c0d6e59e39199f3ac3a93159d4f
BrittStrauvenPXL <116876064+BrittStrauvenPXL@users.noreply.github.com>
2023-12-07 08:53:19 +0100

    Update appTest.js

--- appTest.js
+++ appTest.js
@@ -1,5 +1,8 @@
-require('dotenv').config(path:'TestServer/.env.test');
-const app = require("./app")
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
